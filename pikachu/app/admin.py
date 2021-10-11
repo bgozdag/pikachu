125c02d1126eafe198c6ddc83a6eba4bac76dabc
Buğra Görkem Özdağ <bugra.ozdag@vestel.com.tr>
2021-10-11 17:26:22 +0300

    rename user to barista

--- pikachu/app/admin.py
+++ pikachu/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Coffee, Barista
 
 # Register your models here.
+
+admin.site.register(Coffee)
+admin.site.register(Barista)
--- pikachu/app/models.py
+++ pikachu/app/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 
-class User(models.Model):
-    name = models.TextField()
+class Barista(models.Model):
+    name = models.TextField(max_length=100)
     profile_pic = models.ImageField()
 
 
 class Coffee(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(Barista, on_delete=models.CASCADE)
     date_time = models.DateTimeField('date', auto_now_add=True)
     reviews = models.TextField()
